afb2f5faddb7fa5dd438bba69dee81239d886251
Oluwatoyin Faniyan <56892904+Theedon@users.noreply.github.com>
2024-06-08 21:53:28 +0000

    feat: add util to unmark markdown common to gemini

--- africhain/query_db.py
+++ africhain/query_db.py
@@ -4,10 +4,12 @@ from langchain.chains import create_sql_query_chain
 from langchain_community.utilities import SQLDatabase
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from africhain.utils.replace_md import unmark
+
 db = SQLDatabase.from_uri("sqlite:///data/northwind.db")
 
 
-def query_db(query):
+def query_db(query, db=db):
     print(db)
     print(db.dialect)
     print(db.get_usable_table_names)
@@ -20,12 +22,16 @@ def query_db(query):
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
 
-def question_to_query(question: str):
+def question_to_query(question: str, db=db):
     llm = ChatGoogleGenerativeAI(
         model="gemini-pro", temperature=0.1, google_api_key=google_api_key
     )
     chain = create_sql_query_chain(llm, db)
     response = chain.invoke({"question": "How many customers are there"})
+    clean_response = unmark(response)
+
+
+    print(db.run(clean_response))
     # print(response)
 
-    return response
+    return clean_response
