b30336f63e970feed2cd9046b3eb98be1ac88839
Oluwatoyin Faniyan <56892904+Theedon@users.noreply.github.com>
2024-06-09 19:35:50 +0000

    chore(deps): remove unused libraries

--- africhain/utils/agent.py
+++ africhain/utils/agent.py
@@ -1,17 +1,8 @@
 from langchain import hub
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain_core.prompts import ChatPromptTemplate
 
 
 def build_agent(llm, tools):
-
-    # prompt = ChatPromptTemplate.from_messages(
-    #     [
-    #         ("system", "you're a helpful assistant"),
-    #         ("human", "{input}"),
-    #         ("placeholder", "{agent_scratchpad}"),
-    #     ]
-    # )
 
     prompt = hub.pull("hwchase17/openai-tools-agent")
 
